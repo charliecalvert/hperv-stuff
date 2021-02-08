12732ef1d9a58e75be7ecf0fe18570af5c1b1ea9
Charlie on main wsl hp-desktop <charlie@elvenware.com>
2021-02-07 18:04:08 -0800

    initial commit

---
+++ fo.js
@@ -0,0 +1,4 @@
+let a = 5;
+let b = 10;
+var bar = `Fifteen is ${a + b} and not ${2 * a + b}.`;
+console.log(bar);
---
+++ try.js
@@ -0,0 +1,30 @@
+var fs = require('fs')
+
+var args = process.argv.slice(2);
+console.log(args);
+
+var newIP = "";
+
+fs.readFile(args[0], 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    
+    //newIP = "Host bitbucket.com\n\tHostName " + data;
+    newIP = `Host ${args[1]}.com\n\tHostName ${data}`;
+    console.log('asdf', newIP);
+}); 
+
+fs.readFile('config', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  var go = new RegExp(`Host ${args[1]}.com\n\tHostName .*\n`, 'm')
+  var result = data.replace(go, newIP );
+  console.log(result);
+
+  /* fs.writeFile(someFile, result, 'utf8', function (err) {
+     if (err) return console.log(err);
+  }); */
+});
