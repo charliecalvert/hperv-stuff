5377514dc2f566179da342baa79dcfe5a9ee29ee
charlie on hyper-v mint <charlie@elvenware.com>
2021-02-09 10:29:10 -0800

    it is working and writes comfig but needs code cleanup

--- test.js
+++ test.js
@@ -8,25 +8,41 @@ const CONFIG_NAME = 'config';
 
 console.log(files);
 
+var configData = fs.readFileSync(CONFIG_NAME, 'utf8');
+for (const file of files) {
+  if (file != "README.md") {
+    configData = modifyConfig([`ip-files/${file}`, file, configData]);
+    console.log('RESLT', configData);
+  }
+}
+
+console.log(configData);
+
+fs.writeFile(CONFIG_NAME, configData, 'utf8', function (err) {
+  if (err) return console.log(err);
+});
+
+return;
+
 fs.readFile(CONFIG_NAME, 'utf8', function (err, configData) {
-    if (err) {
-      return console.log(err);
-    }
+  if (err) {
+    return console.log(err);
+  }
 
-    /* for (const file of files) {
-        if (file != "README.md") {
-            configData = modifyConfig([`ip-files/${file}`, file, configData ]);
-            console.log('RESLT', configData);
-        }
-    } */
+  /* for (const file of files) {
+      if (file != "README.md") {
+          configData = modifyConfig([`ip-files/${file}`, file, configData ]);
+          console.log('RESLT', configData);
+      }
+  } */
 
-    const file = 'us01';
-    configData = modifyConfig([`ip-files/${file}`, file, configData ]);
+  const file = 'us01';
+  configData = modifyConfig([`ip-files/${file}`, file, configData]);
 
-    console.log(configData);
-    
-    /* fs.writeFile(CONFIG_NAME, configData, 'utf8', function (err) {
-       if (err) return console.log(err);
-    }); */
-  });
+  console.log(configData);
+
+  /* fs.writeFile(CONFIG_NAME, configData, 'utf8', function (err) {
+     if (err) return console.log(err);
+  }); */
+});
 
